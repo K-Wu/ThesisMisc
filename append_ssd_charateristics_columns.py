6aaf40652b66f763a39a3d850bd4f249569ebc5b
K-Wu <kunwu2@illinois.edu>
2024-09-03 10:04:09 -0500

    upd

--- append_ssd_charateristics_columns.py
+++ append_ssd_charateristics_columns.py
@@ -1,8 +1,11 @@
 import requests
 from lxml import html
+import pandas as pd
+import time
+import os
 
-INPUT_FILENAME = "techpowerup_enterprise_ssds.csv"
-OUTPUT_FILENAME = "techpowerup_enterprise_ssds_with_characteristics.csv"
+INPUT_FILENAME = "techpowerup_enterprise_ssds_with_links.csv"
+OUTPUT_FILENAME = "techpowerup_enterprise_ssds_with_links_and_characteristics.csv"
 
 def test_print_all_enterprise_ssds():
     r=requests.get('https://www.techpowerup.com/ssd-specs/search/?market=2')
@@ -14,6 +17,7 @@ def test_print_one_enterprise_ssds_characteristics():
     doc=html.fromstring(r.content)
     for idx, characteristic in enumerate(doc.xpath('//*[@class="details"]//tr')):
         print(idx, characteristic.xpath('th/text()'), characteristic.xpath('td/text()'))
+        # 8 ['Interface:'] ['PCIe 5.0 x4']
         # 21 ['Type'] ['TLC']
         # 22 ['Technology:'] ['176-layer']
         # 30 ['Read Time (tR):'] ['50 µs']
@@ -25,5 +29,65 @@ def test_print_one_enterprise_ssds_characteristics():
         # 42 ['Random Read:'] ['2,350,000 IOPS']
         # 43 ['Random Write:'] ['150,000 IOPS']
 
+def extract_characteristics_for_one_ssd(url:str, characteristics: dict[str, list[str]]):
+    r=requests.get(url)
+    doc=html.fromstring(r.content)
+    found_keys: set[str] = set()
+    for idx, characteristic in enumerate(doc.xpath('//*[@class="details"]//tr')):
+        if len(characteristic.xpath('th/text()')) == 0:
+            print("Warning: empty characteristics", idx, characteristic.xpath('th/text()'), characteristic.xpath('td/text()'))
+            continue
+        key = characteristic.xpath('th/text()')[0].replace(":", "")
+        value = characteristic.xpath('td/text()')[0]
+        if key in characteristics:
+            characteristics[key].append(value)
+            found_keys.add(key)
+        else:
+            characteristics[key] = [value]
+    
+    for key in characteristics:
+        if key not in found_keys:
+            characteristics[key].append("NotFound")
 
-test_print_one_enterprise_ssds_characteristics()
+def extract_characteristics_for_all_ssds(df: pd.DataFrame, resume_flag: bool):
+    characteristics = {}
+    if resume_flag:
+        for key in ["Interface", "Type", "Technology", "Read Time (tR)", "Program Time (tProg)", "Die Read Speed", "Die Write Speed", "Sequential Read", "Sequential Write", "Random Read", "Random Write"]:
+            characteristics[key] = df[key].to_list()
+            for item in reversed(characteristics[key]):
+                if (item is not None) and (pd.isnull(item) == False):
+                    break
+                characteristics[key].pop()
+    else:
+        for key in ["Interface", "Type", "Technology", "Read Time (tR)", "Program Time (tProg)", "Die Read Speed", "Die Write Speed", "Sequential Read", "Sequential Write", "Random Read", "Random Write"]:
+            characteristics[key] = []
+    try:
+        for idx, row in df.iterrows():
+            if resume_flag and len(characteristics["Interface"]) > idx:
+                continue
+            print("Extracting characteristics for", row["link"])
+            extract_characteristics_for_one_ssd(row["link"], characteristics)
+            time.sleep(1)
+    except Exception as e:
+        print("Exception", e)
+        print("Get error, store scraped data")
+    for key in characteristics:
+        if len(characteristics[key]) != len(df):
+            if len(df) - len(characteristics[key]) > 0:
+                print("Warning: missing characteristics", key, len(characteristics[key]), len(df), characteristics[key])
+                characteristics[key] = characteristics[key] + [None] * (len(df) - len(characteristics[key]))
+            else:            
+                print("Warning: excessive number of characteristics", key, len(characteristics[key]), len(df), characteristics[key])
+                characteristics[key] = characteristics[key][:len(df)]
+        df[key] = characteristics[key]
+
+if __name__ == "__main__":
+    if os.path.exists(OUTPUT_FILENAME):
+        df = pd.read_csv(OUTPUT_FILENAME)
+        extract_characteristics_for_all_ssds(df, True)
+    else:
+        df = pd.read_csv(INPUT_FILENAME)
+        # print(df)
+        extract_characteristics_for_all_ssds(df, False)
+    df.to_csv(OUTPUT_FILENAME)
+    # test_print_one_enterprise_ssds_characteristics()
--- sort_example.cpp
+++ sort_example.cpp
@@ -7,11 +7,12 @@ int main()
 {
     vector<tuple<string, string> > v{{"Alice","227 CSL"},{"Bob","1210 Siebel Center"},{"Charlie", "2120 ECE Building"}};
 
-    sort(v.begin(), v.end(), [](const tuple<string, string>& a, const tuple<string, string>& b) {
+    sort(v.begin(), v.end(), [](const tuple<string, string>& a, const tuple<string, string>& b) { // a's type and b's type can be written as auto
         return get<1>(a) < get<1>(b);
     });
 
-    for (int i = 0; i < v.size(); i++) 
+    // i's type can be written as auto
+    for (int i = 0; i < v.size(); i++)
         cout << get<0>(v[i]) << " " << get<1>(v[i]) << " " << "\n";
         
     return 0;
